83ca4490414386d047c812d50b48a6e85342a7c0
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2026-02-20 16:27:08 +0600

    add sell-property and all-properties pages code

---
+++ src/app/sellproperty/page.jsx
@@ -0,0 +1,10 @@
+import SellPropertyParent from "@/src/components/SellProperty/SellPropertyParent";
+
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-10">
+      <SellPropertyParent></SellPropertyParent>
+    </main>
+  );
+}
